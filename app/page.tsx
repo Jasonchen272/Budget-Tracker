5b1c4a135858f8a1732fd3b9bfaaa429f4fb9c6e
Jasonchen272 <jchen.012004@gmail.com>
2024-01-04 01:58:16 -0600

    Things

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Link,
+  Text,
+  VStack,
+  HStack,
+  Container,
+} from '@chakra-ui/react';
+function HomePage() {
+  return (
+    <Box></Box>
+  );
+}
+
+export default HomePage;
---
+++ app/test/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Link,
+  Text,
+  VStack,
+  Container,
+} from '@chakra-ui/react';
+
+function HomePage() {
+  return (
+    <Text>hello world</Text>
+  );
+}
+
+export default HomePage;
