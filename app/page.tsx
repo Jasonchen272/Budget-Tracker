a4d4685fed514badaa28a9fbd7f55dffc58c0a8b
anthony39844 <ant39844@gmail.com>
2024-01-04 22:32:13 -0600

    f

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '@/components/Header';
 import SubHeader from '@/components/Subheader'
+import Transactions from '@/components/Transactions';
+import TransactionAdder from '@/components/TransactionAdder';
 
 import {
   Box,
@@ -16,14 +18,13 @@ import {
   NumberInput,
   NumberInputField,
 } from '@chakra-ui/react';
-import Transactions from '@/components/Transactions';
-import TransactionAdder from '@/components/TransactionAdder';
+
 function HomePage() {
     const currentDate = new Date();
     const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
   
   return (
-    <Box textAlign={"center"}>
+    <Box textAlign={"center"} bg="gray.700">
           <Header/>
     <SubHeader curr_month={currentMonth} total={0}/>
     <Box textAlign={"center"}>
